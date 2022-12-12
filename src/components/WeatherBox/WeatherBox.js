15eb3d4b8739ed01315a1cdea222b4db6441cd92
Konrad Brożyna <kondzio3002@gmail.com>
2022-12-12 21:15:07 +0100

    Add handleSubmit and handleCityChange

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,11 +1,17 @@
 import PickCity from '../PickCity/PickCity';
 import WeatherSummary from '../WeatherSummary/WeatherSummary';
 import Loader from '../Loader/Loader';
+import { useCallback } from 'react';
 
 const WeatherBox = props => {
+
+  const handleCityChange = useCallback(e => {
+    console.log(e);
+  }, []);
+
   return (
     <section>
-      <PickCity />
+      <PickCity action={handleCityChange} />
       <WeatherSummary />
       <Loader />
     </section>
